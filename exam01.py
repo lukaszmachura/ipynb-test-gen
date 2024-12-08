acc0c51b1e43397c234d94fbcbdfd7a2482596ca
Lukasz Machura <lukasz.machura@gmail.com>
2024-12-08 13:39:36 +0100

    name of exam = name of script

--- exam01.py
+++ exam01.py
@@ -1,4 +1,5 @@
 from ipytest import *
+import sys
 
 # get service handle
 service = connect_to_service()
@@ -7,7 +8,10 @@ service = connect_to_service()
 spreadsheet_id = '17T0Nnea1gwaAUyM7Sy2awiXyti7_2P_uOjbP8bOJxmo'
 
 # exam name
-exam = 'exam01'
+exam = ''
+if not exam:
+    # take the exam name from the conf file (=this file)
+    exam = sys.argv[0].split('/')[-1].strip('.py')
 
 # copy pattern file to individual files
 names_range = 'names!A1:B'
